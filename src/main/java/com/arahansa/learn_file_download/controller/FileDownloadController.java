f488deccacea33cc8b06802005b585284395e76e
arahansa <arahansa@naver.com>
2022-02-16 23:55:14 +0900

    asdf

--- src/main/java/com/arahansa/learn_file_download/controller/FileDownloadController.java
+++ src/main/java/com/arahansa/learn_file_download/controller/FileDownloadController.java
@@ -31,32 +31,25 @@ public class FileDownloadController {
 
     @GetMapping("/down")
     public ResponseEntity<Resource> download() throws IOException {
-        // 파일을 Azure Blob Storage에서 다운 받아온다.
         Resource resource = new InputStreamResource(new URL("https://d2jneiw56ezkg5.cloudfront.net/card/gangnamgu/villa2.png").openStream());
 
         String mimeType = "image/jpeg";
-        // 파일을 다운 받을 때 첨부 파일 형식으로 받을 수 있도록 헤더값을 설정한다.
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Type", mimeType);
         headers.add("Content-Disposition", "attachment; filename='" + "villa2.png" + "'");
         headers.setContentDisposition(ContentDisposition.attachment()
-                // 사용자에게 보여질 파일 이름을 설정한다.
                 .filename("villa2.png", StandardCharsets.UTF_8).build());
-
         return ResponseEntity.ok().headers(headers).contentType(MediaType.parseMediaType(mimeType)).body(resource);
     }
 
     @RequestMapping(value="/zip", produces="application/zip")
     public void zipFiles(HttpServletResponse response) throws IOException {
-
-        //setting headers
         response.setStatus(HttpServletResponse.SC_OK);
         response.addHeader("Content-Disposition", "attachment; filename=\"test.zip\"");
         ZipOutputStream zipOutputStream = new ZipOutputStream(response.getOutputStream());
         List<String> srcFiles = Arrays.asList("https://d2jneiw56ezkg5.cloudfront.net/card/gangnamgu/villa2.png", "https://d2jneiw56ezkg5.cloudfront.net/card/gangnamgu/villa40.png");
-        // package files
         for (String srcFile : srcFiles) {
-            //new zip entry and copying inputstream with file to zipOutputStream, after all closing streams
+
             String substring = srcFile.substring(srcFile.lastIndexOf("/")+1);
             zipOutputStream.putNextEntry(new ZipEntry(substring));
 
